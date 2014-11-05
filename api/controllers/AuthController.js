7d2cd75e5fed88f35a4695c9497c1ded7a80fd2d
Meghan Larson <meghanlarson@benetech.org>
2014-11-05 12:29:33 -0500

    MMLC-80 Start of user auth

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,30 @@
+/**
+ * AuthControllerController
+ *
+ * @description :: Server-side logic for managing Authcontrollers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var passport = require('passport');
+module.exports = {
+	login: function (req, res) {
+    	res.view();
+  	},
+  	process: function(req, res) {
+    	passport.authenticate('local', function(err, user, info) {
+      		if ((err) || (!user)) {
+        		return res.send({
+        			message: 'login failed'
+        		});
+        		res.send(err);
+      		}
+      		req.logIn(user, function(err) {
+        		if (err) res.send(err);
+        		return res.redirect("/");
+      		});
+    	})(req, res);
+  	},
+  	logout: function (req,res){
+		req.logout();
+		res.send('logout successful');
+  	}
+};
