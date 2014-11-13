fbaf5de111abc99d236fc00373bec333363d09e8
John Brugge <john.brugge@gmail.com>
2014-11-13 10:41:37 -0600

    MMLC-88: Wrap the test in Jasmine functions to make it easier to group scenarios and test cases.

--- spec/api_convert_spec.js
+++ spec/api_convert_spec.js
@@ -1,18 +1,26 @@
+// With Frisby installed globally, we need the full path to find it
 var frisby = require('/usr/local/lib/node_modules/frisby');
 
-// For whatever reason, the DNS lookup isn't finding the host name. 
+// For whatever reason, the DNS lookup in my VM isn't finding the host name
+// so using the IP address for now. 
 //var base_url = 'http://mathml-cloud.cloudapp.net';
 var base_url = 'http://23.101.204.234';
 
-frisby.create("Simple ASCII math")
-	.post(base_url + '/mathml/convert', {
-		mathType : 'AsciiMath', 
-		math : 'a^2+b^2=c^2',
-		description : 'true',
-		svg : 'false'
-	})
-	.expectStatus(200)
-	.expectJSON({
-		description : 'a square plus b square equals c square'
-	})
+describe("Simple math conversion", function() {
+	
+	it("handles algebra AsciiMath", function() {
+		frisby.create("Simple ASCII math")
+			.post(base_url + '/mathml/convert', {
+				mathType : 'AsciiMath', 
+				math : 'a^2+b^2=c^2',
+				description : 'true',
+				svg : 'false'
+			})
+			.expectStatus(200)
+			.expectHeaderContains("content-type", "application/json")
+			.expectJSON({
+				description : 'a square plus b square equals c square'
+			})
 	.toss();
+	});
+});
