3e2c9d52e369c02f6fbcbd06111af82dee691d6f
John Brugge <john.brugge@gmail.com>
2014-12-12 16:57:05 -0600

    MMLC-122: Adding other login specs, and cleaning up test data.

--- spec/api_convert_spec.js
+++ spec/api_convert_spec.js
@@ -1,10 +1,6 @@
 // With Frisby installed globally, we need the full path to find it
 var frisby = require('/usr/local/lib/node_modules/frisby');
 
-// For whatever reason, the DNS lookup in my VM isn't finding the host name
-// so using the IP address for now. 
-//var base_url = 'http://mathml-cloud.cloudapp.net';
-//var base_url = 'http://23.101.204.234';
 // Local testing
 var base_url = 'http://localhost:1337';
 
--- spec/login_spec.js
+++ spec/login_spec.js
@@ -1,23 +1,45 @@
 // With Frisby installed globally, we need the full path to find it
 var frisby = require('/usr/local/lib/node_modules/frisby');
 
-// For whatever reason, the DNS lookup in my VM isn't finding the host name
-// so using the IP address for now. 
-//var base_url = 'http://mathml-cloud.cloudapp.net';
-//var base_url = 'http://23.101.204.234';
 // Local testing
 var base_url = 'http://localhost:1337';
 
 describe("User login", function() {
-	
+	// TODO: Need to ensure that this user already exists
 	it("handles an admin user", function() {
 		frisby.create("Admin login")
 			.post(base_url + '/auth/process', {
-				username : 'johnbrugge@benetech.org', 
+				username : 'admin@benetech.org', 
 				password : '123456'
 			})
 			.expectStatus(302)
 			.expectHeaderContains("Location", "/admin")
+	.toss();
+	});
+	
+	// TODO: Need to ensure that this user already exists
+	it("handles a normal user", function() {
+		frisby.create("User login")
+			.post(base_url + '/auth/process', {
+				username : 'user@benetech.org', 
+				password : '123456'
+			})
+			.expectStatus(302)
+			.expectHeaderContains("Location", "/")
+	.toss();
+	});
+	
+	it("handles a login failure", function() {
+		frisby.create("Bad login")
+			.post(base_url + '/auth/process', {
+				username : 'foo@bar.com', 
+				password : 'abcde'
+			})
+			.expectStatus(200)
+			.expectHeaderContains("Location", "/login")
+			expectJSON({
+				message : "login failed"
+			})
 	.toss();
 	});
 });
