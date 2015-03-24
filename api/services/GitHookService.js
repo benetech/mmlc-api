fb3c4f9534a1509949d24f180f1dd21cebf879e1
Meghan Larson <meghanlarson@benetech.org>
2015-03-24 13:08:00 -0400

    Wait for all of the data

--- api/services/GitHookService.js
+++ api/services/GitHookService.js
@@ -2,18 +2,25 @@ var crypto = require('crypto');
 var compare = require('secure-compare');
 var Buffer = require('buffer').Buffer;
 module.exports = {
-    verify_signature: function(payload_body, header_signature, callback) {
+    verify_signature: function(req, callback) {
         if (typeof(process.env.SECRET_TOKEN) == "undefined") {
             callback("Please set the SECRET_TOKEN.");
         } else if (typeof(payload_body) == "undefined") {
             callback("Payload body not set.");
         } else {
-            var signature = 'sha1=' + crypto.createHmac('sha1', process.env.SECRET_TOKEN).update(payload_body.toString()).digest('hex');
-            if (compare(signature, header_signature)) {
-                callback(null);
-            } else {
-                callback("Signatures didn't match!");
-            }
+            var signature = 'sha1=' + crypto.createHmac('sha1', process.env.SECRET_TOKEN);
+            req.on("data", function(data) {
+                signature.update(data).digest('hex');
+            });
+            req.on("end", function() {
+                console.log(signature);
+                console.log(req.headers['x-hub-signature']);
+                if (compare(signature, req.headers['x-hub-signature'])) {
+                    callback(null);
+                } else {
+                    callback("Signatures didn't match!");
+                }
+            });
         }
     }
 };
