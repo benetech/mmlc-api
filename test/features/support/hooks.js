b64860515ca9ebfc721e68ad87773d66685b0fc2
karen chan <karen@karen-chan.com>
2018-12-19 17:39:53 +0000

    Add code to start sails server in tests
    Jasmine and cucumber tests expect a server to be running.  It's best to
    include the code to start and stop the server in the test set up code so
    it is easy to run.  For jasmine tests, we add event handlers for
    jasmineStarted and jasmineDone events.  For cucumber tests, we add
    `BeforeAll` and `AfterAll` handlers.

---
+++ test/features/support/hooks.js
@@ -0,0 +1,20 @@
+const { BeforeAll, AfterAll } = require('cucumber');
+var sails = require('sails');
+
+BeforeAll(function(done) {
+    // Tests expect to run with NODE_ENV=development
+    process.env.NODE_ENV = 'development';
+    sails.lift({
+        // Your sails app's configuration files will be loaded automatically,
+        // but you can also specify any other special overrides here for testing purposes.
+
+        // For example, we might want to skip the Grunt hook,
+        // and disable all logs except errors:
+        hooks: { grunt: false },
+        log: { level: 'info' },
+    }, done);
+});
+
+AfterAll(function(done) {
+    sails.lower(done);
+});
